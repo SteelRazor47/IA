e2effda78367d644a8d77b619c4c43b990f98df0
Leonardo <leonardobenini09@gmail.com>
2020-11-30 12:31:07 +0100

    Added new pages
    Used to check deployment to Github Pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,31 @@
 import React from "react"
-import App from '../components/App.js'
+import { makeStyles } from "@material-ui/core/styles"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import Container from "@material-ui/core/Container"
+import Header from "../components/Header"
+
+const useStyles = makeStyles(theme => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}))
+
+const sections = [
+  { title: "Storia", url: "/history" },
+  { title: "Etica", url: "/ethics" },
+  { title: "Arte", url: "/art" },
+  { title: "Matematica", url: "/maths" },
+]
 
 export default function Home() {
-  return <App />
+  const classes = useStyles()
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="false">
+        <Header title="IA" sections={sections} />
+      </Container>
+    </React.Fragment>
+  )
 }
---
+++ src/pages/maths.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Maths(){
+    return <div>Maths test</div>
+}
