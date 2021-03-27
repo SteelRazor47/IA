235d0d14772005608e51ab455ce57d25ae1bbe81
SteelRazor47 <leonardobenini09@gmail.com>
2021-03-28 00:49:42 +0100

    Created basic impl. of a Table of Contents
    Added plugin gatsby-remark-autolink-headers
    Switched from automatically to programmatically generated pages with gatsby-node.js
    Changed folder name for the pages to avoid the automatic creation
    Added ToC component to the Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,9 +9,39 @@ import "@fontsource/roboto"
 import { context } from "./provider"
 import { Helmet } from "react-helmet"
 import logo from "../assets/chip.svg"
+import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import { makeStyles } from "@material-ui/core/styles"
 
+const useStyles = makeStyles(theme => ({
+  toc: {
+    position: "fixed",
+    left: 'calc(50% + 400px)',
+    top: "110px",
+    maxHeight: "50vh",
+    width: "310px",
+    display: "flex",
+    li: {
+      lineHeight: 0,
+    },
+    marginTop: theme.spacing(3),
+  },
+  div: {
+    overflow: "hidden",
+    overflowY: "scroll",
+  },
+}))
 
-export default function Layout({ children }) {
+function ToC({ children }) {
+  return <ul className={useStyles().toc}>{children}</ul>
+}
+
+function InnerScroll({ children }) {
+  return <div className={useStyles().div}>{children}</div>
+}
+
+export default function Layout({ data }) {
+  const { body, tableOfContents } = data.mdx
   const darkMode = useContext(context).isDark
   const darkTheme = createMuiTheme({
     palette: {
@@ -27,12 +57,37 @@ export default function Layout({ children }) {
       </Helmet>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
-        <Container maxWidth="lg">
+        <Container maxWidth="md">
           <Header title="IA" sections={sections} />
-          {children}
+          {typeof tableOfContents.items === "undefined" ? null : (
+            <ToC>
+              <InnerScroll>
+                <h2>Table of contents</h2>
+
+                {tableOfContents.items.map(i => (
+                  <li key={i.url}>
+                    <a href={i.url} key={i.url}>
+                      {i.title}
+                    </a>
+                  </li>
+                ))}
+              </InnerScroll>
+            </ToC>
+          )}
+
+          <MDXRenderer>{body}</MDXRenderer>
           <Footer />
         </Container>
       </ThemeProvider>
     </React.Fragment>
   )
 }
+
+export const query = graphql`
+  query PostBySlug($slug: String!) {
+    mdx(fields: { slug: { eq: $slug } }) {
+      body
+      tableOfContents
+    }
+  }
+`
