47f3837ce5267f8cbda52e51c38c91f50a5492b6
SteelRazor47 <leonardobenini09@gmail.com>
2021-04-16 12:35:55 +0200

    Updated the technical aspects page with images and more styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,16 +15,30 @@ module.exports = {
       },
     },
     {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "mdxPages",
+        path: `${__dirname}/src/assets/aspetti-tecnici-img`,
+      },
+    },
+    {
       resolve: "gatsby-plugin-mdx",
       options: {
         defaultLayouts: {
           default: require.resolve("./src/components/Layout.js"),
         },
-        gatsbyRemarkPlugins: [`gatsby-remark-autolink-headers`],
+        gatsbyRemarkPlugins: [
+          "gatsby-remark-autolink-headers",
+          "gatsby-remark-images",
+        ],
       },
     },
     "gatsby-theme-material-ui",
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-remark-images",
   ],
   pathPrefix: "/IA",
 }
